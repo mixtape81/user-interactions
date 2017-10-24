a51903b84d9bebcb46e86509745917797af0dbfa
Neha Chaudhary <neha.chaudhary@hotmail.com>
2017-10-24 16:08:08 -0700

    Attempt 8 for Travis

--- tests/database-spec.js
+++ tests/database-spec.js
@@ -4,19 +4,29 @@
 // const request = supertest.agent(server);
 // const { expect } = require('chai');
 const environment = process.env.NODE_ENV;
-const envPath = `.env.${environment}`;
-const env_Vars = require('dotenv').config({ path: envPath });
-const test = require('./tests.js');
-
-describe('Database testing', () => {
-  let db;
-  const config = {
+let config;
+if (environment) {
+  const envPath = `.env.${environment}`;
+  const env_Vars = require('dotenv').config({ path: envPath });
+  config = {
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     dialect: 'postgres'
   };
+} else {
+  config = {
+    database: 'travis_ci_test',
+    username: 'postgres',
+    password: '',
+    dialect: 'postgres'
+  };
+}
 
+const test = require('./tests.js');
+
+describe('Database testing', () => {
+  let db;
   beforeEach(() => {
     db = test.Sequelize(config);
     db.authenticate()
