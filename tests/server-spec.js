3028c8560359fc4c095c6e3e396026474e08fc23
Neha Chaudhary <neha.chaudhary@hotmail.com>
2017-10-24 16:24:13 -0700

    Attempt 10 Travis

--- tests/server-spec.js
+++ tests/server-spec.js
@@ -1,17 +1,11 @@
 const environment = process.env.NODE_ENV;
+const envPath = `.env.${environment}`;
+const env_Vars = require('dotenv').config({ path: envPath });
 const test = require('./tests.js');
-
-if (environment) {
-  const envPath = `.env.${environment}`;
-  const env_Vars = require('dotenv').config({ path: envPath });
-}
-
-const host = process.env.HOSTNAME || '127.0.0.1';
-const port = process.env.PORT || 2000;
 
 describe('server connection test', () => {
   beforeEach(() => {
-    test.server.listen(port);
+    test.server.listen(process.env.PORT);
   });
 
   afterEach(() => {
@@ -21,20 +15,20 @@ describe('server connection test', () => {
 
 describe('/', () => {
   it('should return 200 as response to /', (done) => {
-    test.supertest(host)
+    test.supertest(process.env.HOSTNAME)
       .get('/')
       .expect(200, done);
   });
 
 
   it('should return 404 for everything else', (done) => {
-    test.supertest(host)
+    test.supertest(process.env.HOSTNAME)
       .get('/test')
       .expect(404, done);
   });
 
   it('should say "Hello world!"', (done) => {
-    test.supertest(host)
+    test.supertest(process.env.HOSTNAME)
       .get('/')
       .expect('Hello, world!', done);
   });
