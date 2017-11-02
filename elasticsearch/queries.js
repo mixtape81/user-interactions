8a47ad73362b8001a0f73567ae34d3378e503b07
Neha Chaudhary <neha.chaudhary@hotmail.com>
2017-11-02 00:11:47 -0700

    clean up elasticsearch code

--- elasticsearch/queries.js
+++ elasticsearch/queries.js
@@ -1,22 +1,6 @@
 const { client } = require('./');
 
-// can use promises instead of callbacks
-
 const addDocumentsInBulk = (index, type, body) => client.bulk({ index, type, body });
-
- // client.index({
- //     index: 'blog',
- //     id: '1',
- //     type: 'posts',
- //     body: {
- //         "PostName": "Integrating Elasticsearch Into Your Node.js Application",
- //         "PostType": "Tutorial",
- //         "PostBody": "This is the text of our tutorial about using Elasticsearch in your Node.js application.",
- //     }
- // }, function(err, resp, status) {
- //     console.log(resp);
- // });
-
 
 module.exports = {
   addDocumentsInBulk
